c1f1085b00116bb6037c07d7cb61252308becc27
Student <rubenag025@gmail.com>
2020-09-23 23:22:31 -0500

    add array

---
+++ arrays_1/mergemeetingtimes.js
@@ -0,0 +1,13 @@
+meetings =   [
+    { startTime: 0,  endTime: 1 },
+    { startTime: 3,  endTime: 5 },
+    { startTime: 4,  endTime: 8 },
+    { startTime: 10, endTime: 12 },
+    { startTime: 9,  endTime: 10 },
+  ]
+
+//TODO: How could i determine the overlapping time periods?
+
+for(i = 0; i < meetings.length; i++){
+    
+}
