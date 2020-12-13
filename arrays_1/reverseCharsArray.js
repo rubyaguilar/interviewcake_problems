742eb0276d9d88cd3cae6d423acdbc42756b824f
rubenag025 <rubenag025@gmail.com>
2020-12-12 18:46:41 -0600

    immutable array function

--- arrays_1/reverseCharsArray.js
+++ arrays_1/reverseCharsArray.js
@@ -1,7 +1,28 @@
+
+
+
+
 function reverse(arrayOfChars) {
 
+    arrayOfChars.split("")
+
+    if(arrayOfChars.length === 0 ){ //empty string 
+        return arrayOfChars
+    } else {
+        var L = 0; //left most 
+        var R = arrayOfChars.length-1; 
+
+        while ( L !== Math.floor( (arrayOfChars.length-1) / 2) )  { 
+            var tempLeft = arrayOfChars[L]
+            arrayOfChars[L] = arrayOfChars[R]
+            arrayOfChars[R] = tempLeft
+            L++
+            R--
+        }
+    }
     
-    
+    return arrayOfChars 
+}
   
-  }
-  
+
+console.log(reverse("ABCDE"))
