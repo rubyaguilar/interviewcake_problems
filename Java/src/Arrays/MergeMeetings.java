adb61e778441020fcca565aeb0610cf30219324a
rubenag025 <rubenag025@gmail.com>
2020-12-21 10:43:48 -0600

    adding java package

---
+++ Java/src/Arrays/MergeMeetings.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class MergeMeetings {
+
+    private static int[] mergeArrays(int[] myArray, int[] alicesArray) {
+
+        return myArray;
+    }
+
+    public static void main(String[] args) {
+
+        int[] myArray = new int[]{3, 4, 6, 10, 11, 15};
+        int[] alicesArray = new int[]{1, 5, 8, 12, 14, 19};
+
+
+    }
+
+
+}
