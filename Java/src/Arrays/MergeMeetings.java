0ae91a52081a29bbb9940b8a406c8465a6b7527a
rubenag025 <rubenag025@gmail.com>
2020-12-21 19:36:35 -0600

    remove .idea file

--- Java/src/Arrays/MergeMeetings.java
+++ Java/src/Arrays/MergeMeetings.java
@@ -6,13 +6,24 @@ public class MergeMeetings {
 
     private static int[] mergeArrays(int[] myArray, int[] alicesArray) {
 
+        for(int i = 0; i < myArray.length; i++){
+            for(int j = 0; j < myArray.length; j++){
+                if(
+
+
+                )
+            }
+        }
+
         return myArray;
     }
 
     public static void main(String[] args) {
 
-        int[] myArray = new int[]{3, 4, 6, 10, 11, 15};
-        int[] alicesArray = new int[]{1, 5, 8, 12, 14, 19};
+        int[] myArray = new int[] {3, 4, 6, 10, 11, 15};
+        int[] alicesArray = new int[] {1, 5, 8, 12, 14, 19};
+
+
 
 
     }
