aa72d2ee96121c33003dabcb6b71b73036823e76
rubenag025 <rubenag025@gmail.com>
2020-12-29 19:19:11 -0600

    merge meetings

--- JavaScript/arrays_1/mergeArrays.js
+++ JavaScript/arrays_1/mergeArrays.js
@@ -1,8 +1,56 @@
-function smallestGroups(array){
 
+function mergeArrays(myArray, alicesArray) {
+    var alicesIndex = 0; 
+    var myIndex = 0; 
 
-  
+    var merged = []
+    var mergedIndex = 0; 
+
+    if(myArray.length > 0 && alicesArray.length > 0) { //both arrays greater than 0 
+        while(mergedIndex < (myArray.length + alicesArray.length)){
+            var alicesCurrentVal = alicesArray[alicesIndex]
+            var myCurrentval = myArray[myIndex]
+
+            if(alicesIndex < alicesArray.length && myIndex < myArray.length){
+                if(myCurrentval < alicesCurrentVal){
+                    merged[mergedIndex] = myCurrentval;
+                    myIndex++
+                } else {
+                    merged[mergedIndex] = alicesCurrentVal
+                    alicesIndex++
+                }
+                mergedIndex++;
+            } else {
+                //we have reached the end of an array 
+                if(myIndex === myArray.length){
+                    while(alicesIndex !== alicesArray.length){
+                        merged[mergedIndex] = alicesArray[alicesIndex]
+                        mergedIndex++
+                        alicesIndex++
+                    }
+                    return merged; 
+                } else {
+                    while(myIndex !== myArray.length){
+                        merged[mergedIndex] = myArray[myIndex]
+                        mergedIndex++
+                        myIndex++
+                    }
+                    return merged; 
+                }
+            }
+            
+        }
+    } else {
+        if(myArray.length > alicesArray.length){
+            return myArray
+        } else {
+            return alicesArray
+        }
+    }
+
+    return merged;
 }
+const myArray = [2, 4, 6, 8];
+const alicesArray = [1, 7];
 
-
-smallestGroups([1.01, 1.01, 3.0, 2.7, 1.99, 2.3, 1.7])
+console.log(mergeArrays(myArray, alicesArray));
